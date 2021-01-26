e77a47df2b590d3904310931ac02449a76efe4f8
reh42 <reharbison42@gmail.com>
2021-01-25 19:57:44 -0600

    Becca changes

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -27,4 +27,29 @@ public class BalancedBracketsTest {
         assertTrue(BalancedBrackets.hasBalancedBrackets("[launchcode]"));
     }
 
+    @Test
+    public void bracketFirstReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]launchcode"));
+    }
+
+    @Test
+    public void missingClosingBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("["));
+    }
+
+    @Test
+    public void missingOpeningBracketReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]"));
+    }
+
+    @Test
+    public void reverseBracketsReturnsFalse(){
+        assertFalse(BalancedBrackets.hasBalancedBrackets("]["));
+    }
+
+    @Test
+    public void multipleBalenceBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[][]"));
+    }
+
 }
