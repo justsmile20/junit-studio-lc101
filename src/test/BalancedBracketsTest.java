4378f3f08e8560d4c49ce6ba87790846fde8eee0
dharr <dharriel3@hotmail.com>
2021-01-25 19:33:15 -0600

    This is our first test.

--- src/test/BalancedBracketsTest.java
+++ src/test/BalancedBracketsTest.java
@@ -1,5 +1,6 @@
 package test;
 
+import main.BalancedBrackets;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -12,5 +13,18 @@ public class BalancedBracketsTest {
         assertEquals(true, true);
     }
 
+    @Test
+    public void onlyBracketsReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[]"));
+    }
+
+    @Test
+    public void emptyStringReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets(""));
+    }
+    @Test
+    public void wordsInBracketReturnsTrue(){
+        assertTrue(BalancedBrackets.hasBalancedBrackets("[launchcode]"));
+    }
 
 }
